d50832a457a6c43e3fcc6a783707ede3768aabc2
Craig J Perry <craigp84@gmail.com>
2024-11-04 11:10:16 +0000

    chore: setup leptos dx

--- src/app.rs
+++ src/app.rs
@@ -1,5 +1,5 @@
-use leptos::*;
 use crate::config::Config;
+use leptos::*;
 
 #[component]
 pub fn App() -> impl IntoView {
@@ -13,18 +13,21 @@ pub fn App() -> impl IntoView {
                 <img src="public/cmm-logo.svg" class="logo cmm" alt="logo" />
             </div>
 
-            {move || if show_config_editor.get() {
-                let config = Config::new();
-                view! {
-                    <ConfigurationEditor config={config.file_exclude_patterns().clone()} />
-                }} else {
+            {move || {
+                if show_config_editor.get() {
+                    let config = Config::new();
+                    view! { <ConfigurationEditor config=config.file_exclude_patterns().clone() /> }
+                } else {
                     view! {
                         <form class="row">
-                            <button on:click=move |_| set_show_config_editor.update(|v| *v = !*v)>"Configuration"</button>
+                            <button on:click=move |_| {
+                                set_show_config_editor.update(|v| *v = !*v)
+                            }>"Configuration"</button>
                         </form>
-                    }.into_view()
+                    }
+                        .into_view()
                 }
-            }
+            }}
         </main>
     }
 }
@@ -35,13 +38,17 @@ pub fn ConfigurationEditor(config: Vec<String>) -> impl IntoView {
         <div>
             <h2>"Configuration Editor"</h2>
             <form class="config-editor">
-                {config.iter().map(|pattern| view! {
-                    <div>
-                        <input type="text" value={pattern} />
-                        <button>"Remove"</button>
-                    </div>
-                }).collect::<Vec<_>>()}
-                <div>
+                {config
+                    .iter()
+                    .map(|pattern| {
+                        view! {
+                            <div>
+                                <input type="text" value=pattern />
+                                <button>"Remove"</button>
+                            </div>
+                        }
+                    })
+                    .collect::<Vec<_>>()} <div>
                     <button>"Add"</button>
                 </div>
             </form>
--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,13 @@ mod app;
 use app::*;
 use leptos::*;
 
-mod orchestrator;
 mod config;
+mod orchestrator;
 
 fn main() {
     console_error_panic_hook::set_once();
+    _ = console_log::init_with_level(log::Level::Debug);
     mount_to_body(|| {
-        view! {
-            <App/>
-        }
+        view! { <App /> }
     })
 }
